62efd6d0f6dc966107f28453b4ac9a8e0eeabd36
Sloppy Lopez <sloppy@sloppylopez.com>
2026-01-29 00:35:03 +0100

    Refactor PostmanEchoClient to use @RequestParam for query parameters
    - Updated the getUser method to utilize @RequestParam instead of @PathVariable for improved clarity and adherence to RESTful practices.
    - Adjusted the @GetMapping annotation to remove the query parameter from the URL path, aligning with standard query parameter usage.

--- examples/spring-boot-example/src/main/java/example/PostmanEchoClient.java
+++ examples/spring-boot-example/src/main/java/example/PostmanEchoClient.java
@@ -2,15 +2,15 @@ package example;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "postmanEchoClient", url = "${app.postmanecho.url}")
 public interface PostmanEchoClient {
     
-    @GetMapping("/get?id={id}")
-    String getUser(@PathVariable("id") int id);
+    @GetMapping("/get")
+    String getUser(@RequestParam("id") int id);
 
     @PostMapping(value = "/post", consumes = "application/xml")
     String postXml(@RequestBody String xmlBody);
